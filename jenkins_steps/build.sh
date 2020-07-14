d0f8b34e33d322936d3baf8271ef250a577baa75
noussair zarrouk <noussair333@gmail.com>
2020-07-14 14:06:00 +0100

    pipe12

--- jenkins_steps/build.sh
+++ jenkins_steps/build.sh
@@ -1,4 +1,5 @@
-sudo docker login -u noussair333 -p 'DjPvzkdCTP7mqSs'
+PASS='DjPvzkdCTP7mqSs'
+sudo docker login -u noussair333 -p $PASS
 git clone https://gitlab.com/team_devops_pfe/pfespringangular.git kanban
 cd kanban
 sudo docker-compose build
