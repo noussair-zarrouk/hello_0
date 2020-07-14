fa4f1504367a7c7a844e69945b9de9e6f7d2b9f8
noussair zarrouk <noussair333@gmail.com>
2020-07-14 14:18:22 +0100

    pipe14

--- jenkins_steps/build.sh
+++ jenkins_steps/build.sh
@@ -1,6 +1,10 @@
 echo 'DjPvzkdCTP7mqSs' > ~/my_password.txt 
-cat ~/my_password.txt | docker login --username noussair333 --password-stdin
-sudo docker login -u noussair333 -p $PASS
+cat ~/my_password.txt | sudo docker login --username noussair333 --password-stdin
+#sudo docker login -u noussair333 -p $PASS
+echo '{
+  "Username": "noussair333",
+  "Secret": "DjPvzkdCTP7mqSs"
+}'
 git clone https://gitlab.com/team_devops_pfe/pfespringangular.git kanban
 cd kanban
 sudo docker-compose build
