1338f21ce49202be6335dd24fb3abb2465148575
noussair zarrouk <noussair333@gmail.com>
2020-07-14 14:10:10 +0100

    pipe13

--- jenkins_steps/build.sh
+++ jenkins_steps/build.sh
@@ -1,4 +1,5 @@
-PASS='DjPvzkdCTP7mqSs'
+echo 'DjPvzkdCTP7mqSs' > ~/my_password.txt 
+cat ~/my_password.txt | docker login --username foo --password-stdin
 sudo docker login -u noussair333 -p $PASS
 git clone https://gitlab.com/team_devops_pfe/pfespringangular.git kanban
 cd kanban
