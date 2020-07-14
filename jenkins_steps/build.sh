299f1d9629eb1eb5a9ae42c70c5b2e52cbc37e81
noussair zarrouk <noussair333@gmail.com>
2020-07-14 12:05:14 +0100

    pipe7

--- jenkins_steps/build.sh
+++ jenkins_steps/build.sh
@@ -1,4 +1,3 @@
-#docker login -u noussair333 -p DjPvzkdCTP7mqSs
-echo $PASS
-#docker-compose build
-#docker-compose push
+docker login -u noussair333 -p $PASS
+docker-compose build
+docker-compose push
